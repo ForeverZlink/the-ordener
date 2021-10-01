03c7e03d1c7df8b98347fd070b6c61c3dd105bf0
St;ZlinkG <carloscunha080@gmail.com>
2021-09-30 21:42:25 -0300

    tirei o ordernador do código principal e criei um metódo para ele

--- the_ordener/Program.cs
+++ the_ordener/Program.cs
@@ -4,32 +4,29 @@ using System.Collections.Generic;
 namespace the_ordener
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] lista = new int [] { 2, 1, 4, 3, 1, 45, 9 };
-            Console.WriteLine($"Tamanho do array {lista.Length}");
-            List<int> lista_ordenada = new List<int> ();
-            int contador = 0;
 
-            Console.Write("Array para ordenar:");
-            foreach (int valor in lista)
+    { 
+        static List<int> Ordernar_array_do_menor_para_maior (int[] array_para_ordernar )
+        {
+            List<int> lista_ordenada = new List<int>();
+            int contador = 0;
+            foreach (int valor in array_para_ordernar)
             {
-                Console.Write($"{valor} ");
+
                 if (contador == 0)
                 {
                     lista_ordenada.Add(valor);
                 }
                 else
                 {
-                    int ultimo_elemento_index = lista_ordenada.ToArray().Length -1;
-                    if (valor>= lista_ordenada[ultimo_elemento_index])
+                    int ultimo_elemento_index = lista_ordenada.ToArray().Length - 1;
+                    if (valor >= lista_ordenada[ultimo_elemento_index])
                     {
                         lista_ordenada.Add(valor);
                     }
                     else
                     {
-                        foreach(int valor_ordenado in lista_ordenada)
+                        foreach (int valor_ordenado in lista_ordenada)
                         {
                             if (valor <= valor_ordenado)
                             {
@@ -37,22 +34,30 @@ namespace the_ordener
                                 int index = lista_ordenada.IndexOf(valor_ordenado);
                                 lista_ordenada.Insert(index, valor);
                                 break;
-                                    
+
                             }
                         }
                     }
+
                 }
                 contador++;
-                
-                
             }
-            Console.WriteLine("");
-            Console.Write("Array ordenado: ");
-            foreach (int valor_order in lista_ordenada)
-            {
-                Console.Write($"{valor_order} ");
-            }
-            Console.ReadLine();
+
+            return lista_ordenada;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int[] lista = new int [] { 2, 1, 4, 3, 1, 45, 9 };
+            Console.WriteLine($"Tamanho do array {lista.Length}");
+            List<int> lista_ordernada = Ordernar_array_do_menor_para_maior(lista);
+            
+            Console.Write("Array para ordenar:");
+            
+                
+            
 
             
 
